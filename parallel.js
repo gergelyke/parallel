9a4279c771f6ee8bdf8d639302f2876209ad4867
Gergely Nemeth <mail@nemethgergely.com>
2013-05-04 21:13:53 +0200

    init

---
+++ parallel.js
@@ -0,0 +1,44 @@
+var Parallel = (function () {
+
+	var run,
+		ready,
+		partial,
+		readyCounter = 0,
+		parallelCount = 0,
+		callback,
+		results = [];
+
+	partial = function partial(func) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		return function() {
+			var allArguments = args.concat(Array.prototype.slice.call(arguments));
+			return func.apply(this, allArguments);
+		};
+	}
+
+	ready = function ready (order, data) {
+		console.log(order, data)
+		results[i] = data;
+		readyCounter++;
+		if(readyCounter === parallelCount) {
+			results.sort();
+			callback(null, results)
+		}
+	}
+
+
+	run = function run (fns, cbk) {
+		callback = cbk;
+		parallelCount = fns.length;
+		readyCounter = 0;
+
+		for (i = 0; i < fns.length; i++) {
+			fns[i](partial(ready, i));
+		}
+	};
+
+	return {
+		run: run
+	};
+});
+
